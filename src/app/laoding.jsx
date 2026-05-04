8c2e3a106b802c74d434970d6db1c48aa955db27
dulal71 <ahmeddulal4211@gmail.com>
2026-05-04 17:47:05 +0600

    add leading

---
+++ src/app/laoding.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const laoding = () => {
+    return (
+        <div className='h-[70vh] flex justify-center items-center'>
+          <h1>loading...</h1>  
+        </div>
+    );
+};
+
+export default laoding;
