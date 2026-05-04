19be1fd6f51631f5ea86ba398652ea49e5cdc757
dulal71 <ahmeddulal4211@gmail.com>
2026-05-04 17:39:31 +0600

    add new release section

--- src/components/NewRelaases.jsx
+++ src/components/NewRelaases.jsx
@@ -9,10 +9,12 @@ const NewRelaases = async() => {
   const allCourses =await  getData()   
     return (
         <div>
+                       <h2 className="text-3xl md:text-4xl font-extrabold text-center mt-10 mb-6 text-black">New Releases
+</h2>
          {
-            allCourses.slice(6, 12).map(courses=><div key={courses.id} className='my-24 max-w-7xl mx-auto space-y-3'>
-                
-                <div className='flex justify-between items-center bg-white shadow p-3'>
+            allCourses.slice(6, 12).map(courses=><div key={courses.id} className=' max-w-7xl mx-auto my-10'>
+  
+                <div className='flex flex-col gap-2 mb-2 md:gap-0 md:flex-row justify-between items-center bg-white shadow p-3 rounded-md transition-transform duration-500 hover:scale-105'>
 <div className='flex  gap-3'>
 
 <Image src={courses.image} width={200}
@@ -20,7 +22,7 @@ height={200} alt={courses.title}></Image>
 <div>
     <h3 className='font-semibold mb-2 text-xl'>{courses.title}</h3>
 <h4 className='font-semibold'>{courses.instructor}</h4>
-<div className='flex gap-5'>
+<div className='flex gap-5 flex-col md:flex-row'>
     <p className='bg-amber-200 px-2 rounded-md'>{courses.level}</p>
 <p className='flex items-center gap-2'><FiWatch size={20}></FiWatch>{courses.duration}</p>
 
